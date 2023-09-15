f297d085acd3e7c5c94f32a33e4befbb642700c3
Agustin <94864847+IIAGUSII@users.noreply.github.com>
2023-09-15 20:04:59 -0300

    component menu 2 and 3

---
+++ src/assets/components/HamburgerDeployThree.jsx
@@ -0,0 +1,17 @@
+import "../../styles/HamburgerDeployThree.css";
+
+function HamburgerDeployThree() {
+  return (
+   <section className="hamburger-deploy">
+    <div className="hamburger-filter">
+      <h2 className="textH2">MENÚ<br/>COFFE</h2>
+      <div className="selectorHambur">
+        <button className="buttonHambur">CAFÉ</button>
+        <button className="buttonHambur">COMIDAS</button>
+      </div>
+    </div>
+   </section>
+  );
+}
+
+export default HamburgerDeployThree;
---
+++ src/assets/components/HamburgerDeployTwo.jsx
@@ -0,0 +1,16 @@
+import "../../styles/HamburgerDeployTwo.css";
+
+function HamburgerDeployTwo() {
+  return (
+    <section className="hamburger-deploy">
+    <div className="hamburger-filter">
+      <div className="selectorHambur">
+        <button className="buttonHambur">CAFETERAS</button>
+        <button className="buttonHambur">CAFÉ MOLIDO</button>
+      </div>
+    </div>
+    </section>
+  );
+}
+
+export default HamburgerDeployTwo;
