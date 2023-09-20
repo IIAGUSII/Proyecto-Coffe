0d9d67f040527d70458c7923f7863938c312ea1b
Ariadna <mancillaariadna@gmail.com>
2023-09-20 17:58:12 -0300

    corrección de rutas

--- src/assets/components/Navegacion/Header.jsx
+++ src/assets/components/Navegacion/Header.jsx
@@ -1,13 +1,12 @@
-
-import '../../styles/Header.css';
-import Nav from './Nav';
-import { useState } from 'react';
-function Header (setHambur){
-    return(
-        <header className='section-header'>
-            <Nav></Nav>
-        </header>
-    )
+import "./Header.css";
+import Nav from "./Nav";
+import { useState } from "react";
+function Header(setHambur) {
+  return (
+    <header className="section-header">
+      <Nav></Nav>
+    </header>
+  );
 }
 
-export default Header 
+export default Header;
--- src/assets/components/TodosLosProductos/ProdCoffee.jsx
+++ src/assets/components/TodosLosProductos/ProdCoffee.jsx
@@ -1,4 +1,4 @@
-import "../../Styles/ProdCoffee.css";
+import "./ProdCoffee.css";
 import { useEffect, useState } from "react";
 import { useContext } from "react";
 import { UserContext } from "../../../../ProductsContext.jsx";
